017b3f19d0992e8270f96ef5f9ab4fcc101f196a
Sumit4509 <104025799+Sumit4509@users.noreply.github.com>
2024-02-28 21:54:28 +0530

    Add files via upload

---
+++ conditional_statement.java
@@ -0,0 +1,67 @@
+import java.util.*;
+public class conditional_statement {
+    public static void main(String[] args) {
+/*   
+        // check if person is adult or not
+        Scanner sc= new Scanner(System.in);
+        int age = sc.nextInt();
+        if(age>=18){
+            System.out.println("adult");
+
+        }
+        else{
+            System.out.println("not an adult");
+        }
+*/
+/* 
+        // check if no is even or odd 
+        Scanner sc= new Scanner(System.in);
+        int x= sc.nextInt();
+        if(x%2==0){
+            System.out.println("x is even");
+        }
+        else{
+            System.out.println("x is odd");
+        }
+*/
+
+/* 
+
+        // check if two no is equal greater or less than
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a==b){
+            System.out.println("a is equal to b");
+        }
+        else if(a>b){
+            System.out.println("a is greater than b");
+        }
+        else{
+            System.out.println("b is greater than a");
+        }
+*/  
+
+        // using switch print if x is 1 then hello if x is 2 print namaste if x is 3 print bonjour 
+
+/* 
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        switch (x) {
+            case 1:
+                System.out.println("hello");
+                break;
+            case 2:
+                System.out.println("Namaste");
+                break;
+            case 3:
+                System.out.println("Bonjour");
+                break;
+        
+            default:
+            System.out.println("please enter between 1 to 3");
+                
+        }
+*/
+    }
+}
