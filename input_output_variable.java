9914f8781cd7171969deac8bd3977c00f2d200e2
Sumit4509 <104025799+Sumit4509@users.noreply.github.com>
2024-02-28 20:55:07 +0530

    Add files via upload

---
+++ input_output_variable.java
@@ -0,0 +1,49 @@
+// import java.util.*;
+
+public class input_output_variable {
+    public static void main(String[] args) {
+        
+        // Output 
+/* 
+         // print will just display the value inside it
+        System.out.print("Hello World!"); 
+        // println is used to display the data and move to next line
+        System.out.println("Hello World!"); 
+        // \n is used for line spacing 
+        System.out.print("Hello \nWorld!"); 
+        System.out.println(" ");
+
+        // print pattern
+        System.out.println("*"); 
+        System.out.println("**"); 
+        System.out.println("***"); 
+        System.out.println("****"); 
+*/
+
+/* 
+        // variables  since java is typed language so we have to assign the datatype before creating variables 
+
+        int a=10;
+        int b=30;
+        int sum = a+b;
+        System.out.println(sum);
+
+        // there are two types of datatypes first is premetive and second is non premetive inside premetive 8 type of datatype like boolean,int,float,byte,char,double,short,long and in non premetive sting,array,classes,object, interface come 
+*/
+
+/* 
+        // for taking input first import util library
+        Scanner sc = new Scanner(System.in);
+        // here we have created the class 
+        String name = sc.next();
+        // this will only take one token 
+        System.out.println(name);
+        // this will take whole line
+        int age = sc.nextInt();
+        System.out.println(age);
+
+*/
+        
+    }
+}
+   
