6d9ab7b66b87e0d7ebc9fbae7774e9eabfc63e57
Sumit4509 <104025799+Sumit4509@users.noreply.github.com>
2024-03-09 17:02:20 +0530

    Adding question_set1 file

---
+++ question_set1.java
@@ -0,0 +1,131 @@
+import java.util.*;
+public class question_set1 {
+
+// function for 1
+    // public static float average(float n1,float n2,float n3){
+    //     float sum = n1+n2+n3;
+    //     float avr=sum/3;
+    //     return avr;
+    // }
+
+// function for 2
+    // public static int sum(int n ){
+    //     int sum = 0 ;
+    //     for(int i =1;i<=10;i=i+2){
+    //         sum+=i;
+    //     }
+    //     return sum ;
+    // }
+
+// function for 3 
+    // public static void greater(float n1,float n2){
+    //     if(n1>n2){
+    //         System.out.println(n1+" is greater than "+n2);
+    //     }
+    //     else{
+    //         System.out.println(n2+" is greater than "+n1);
+    //     }
+    //     return ;
+    // }  
+    
+// function for 4
+    // public static double circumference(double r){
+    //     double circum = 2*Math.PI*r ;
+    //     return circum;
+    // }
+
+// function for 5
+    // public static void eligible(int a){
+    //     if(a>18){
+    //         System.out.println("You are eligible");
+    //     }
+    //     else{
+    //         System.out.println("You are not eligible");
+    //     }
+    //     return;
+    // }
+
+// function for 8
+    // public static double power(int x, int n ){
+    //     double ans = Math.pow(x, n);
+    //     return ans;
+    // }
+
+
+    public static void main(String[] args) {
+        
+//1. Enter 3 numbers from the user & make a function to print their average.
+        
+        Scanner sc = new Scanner(System.in);
+        // float n1 = sc.nextFloat();
+        // float n2 = sc.nextFloat();
+        // float n3 = sc.nextFloat();
+        // float average = average(n1,n2,n3);
+        // System.out.println(average);
+// 2. Write a function to print the sum of all odd numbers from 1 to n.
+        
+        // int  n = sc.nextInt();
+        // int sum =sum(n);
+        // System.out.println(sum);
+        
+// 3. Write a function which takes in 2 numbers and returns the greater of those two.
+
+        // float num1= sc.nextFloat();
+        // float num2= sc.nextFloat();
+        // greater(num1,num2);
+
+// 4. Write a function that takes in the radius as input and returns the circumference of a circle.
+
+        // float radius = sc.nextFloat();
+        // double circum =circumference(radius);
+        // System.out.println(circum);
+
+
+// 5. Write a function that takes in age as input and returns if that person is eligible to vote or not. A person of age > 18 is eligible to vote.
+
+        // int age =sc.nextInt();
+        // eligible(age);
+
+
+// 6. Write an infinite loop using do while condition
+
+        // int a=1;
+        // do{
+        //     System.out.println("THIS IS INFINITE LOOP");
+        // }
+        // while(a<5);
+
+// 7. Write a program to enter the numbers till the user wants and at the end it should display the count of positive, negative and zeros entered. 
+
+        // int possitive= 0;
+        // int negative= 0 ;
+        // int zero=0 ;
+        // char choice;
+        // do{
+        //     int num = sc.nextInt();
+        //     if(num==0){
+        //         zero++;
+        //     }
+        //     else if (num<0){
+        //         negative++;
+        //     }
+        //     else{
+        //         possitive++;
+        //     }
+        //     System.out.print("Do you want to enter another number? (y/n): ");
+        //     choice =sc.next().charAt(0);
+        // }
+        // while(choice=='y');
+        // System.out.println("No of possitive value : "+possitive);
+        // System.out.println("No of negative value : "+negative);
+        // System.out.println("No of zero value : "+zero);
+
+// 8. Two numbers are entered by the user, x and n. Write a function to find the value of one number raised to the power of another i.e xn.
+
+        // int x =sc.nextInt();
+        // int n =sc.nextInt();
+        // double ans = power(x, n);
+        // System.out.println(ans);
+
+    }
+}
